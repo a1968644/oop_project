4e4301a90c6ad91264df8e9e6dec24ad821af004
Emily <emilychao5@outlook.com>
2025-10-14 09:45:27 +1030

    added cropstate class and its derived classes, planted, harvestable and harvested

---
+++ test.cpp
@@ -0,0 +1,5 @@
+// git clone https://<username>:<Personal Access Token>@github.com/a1968644/oop_project.git
+
+// git clone https://a1968644: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@github.com/a1968644/oop_project.git
+
+// How to create a Personal Access token: https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens#creating-a-personal-access-token-classic
