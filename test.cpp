2549d630c5a2d3335132c4da0c0e7dc3ac595b20
Emily <emilychao5@outlook.com>
2025-10-16 21:33:16 +1030

    finished all seasons - waiting to fill gaps tomorrow morning

--- test.cpp
+++ test.cpp
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
 
 // After changing branch, we have to code "git pull"
 // If "git push" , does not work then cod "git push -u origin main", "main is
 // name of branch"
-=======
 // git clone https://<username>:<Personal Access Token>@github.com/a1968644/oop_project.git
 
 // git clone https://a1968644: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@github.com/a1968644/oop_project.git
 
 // How to create a Personal Access token: https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens#creating-a-personal-access-token-classic
->>>>>>> 4e4301a90c6ad91264df8e9e6dec24ad821af004
